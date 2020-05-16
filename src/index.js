4eca909c475b99e15ff4806743f6f62294564440
Hüsnü Mert POLAT <husnumertpolatt@gmail.com>
2020-05-16 18:09:48 +0300

    port fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import UsersignUpPage from './UserSignUpPage'
 import * as serviceWorker from './serviceWorker';
 
