0f5104a0fa16206622054d2cf7b6bb87ac00c237
Hüsnü Mert POLAT <husnumertpolatt@gmail.com>
2020-05-22 02:16:47 +0300

    request control

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import UsersignUpPage from './UserSignUpPage'
+import UsersignUpPage from './pages/UserSignUpPage'
 import * as serviceWorker from './serviceWorker';
 import './bootstrap-override.scss';
 
