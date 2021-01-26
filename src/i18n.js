32fb3631aa27161cf255a5fd89e77bcbfb447ae5
Hüsnü Mert POLAT <husnumertpolatt@gmail.com>
2021-01-26 22:53:28 +0300

    Authentication

--- src/i18n.js
+++ src/i18n.js
@@ -26,7 +26,7 @@ i18n.use(initReactI18next).init({
             }
         }
     },
-    fallbackLng: 'tr',
+    fallbackLng: 'en',
     ns: ['translations'],
     defaultNS: 'translations',
     keySeparator: false,
