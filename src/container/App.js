ce5c950f00d332f0328241c86bf1539a2393168d
MertPolat <husnumertpolatt@gmail.com>
2021-04-03 14:13:08 +0300

    React Router added.

--- src/container/App.js
+++ src/container/App.js
@@ -3,16 +3,22 @@ import ApiProgress from '../shared/ApiProgress';
 import UserSignUpPage from '../pages/UserSignUpPage';
 import LoginPage from '../pages/LoginPage';
 import LanguageSelector from '../components/LanguageSelector';
+import HomePage from '../pages/HomePage';
+import UserPage from '../pages/UserPage';
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="row">
-      <div className="col">
-          <UserSignUpPage />
-      </div>
-      <div className="col">
-          <LoginPage />
-      </div>
+    <div>
+      <HashRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={UserSignUpPage} />
+        <Route path="/user/:username" component={UserPage} />
+        <Redirect to="/" />
+        </Switch>
+      </HashRouter>
       <LanguageSelector />
     </div>
   );
