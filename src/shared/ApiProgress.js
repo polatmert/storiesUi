70c06df511319e1179bbbf8a90455ac9d50fdb78
MertPolat <husnumertpolatt@gmail.com>
2021-04-07 20:30:25 +0300

    componentWillUnmount

--- src/shared/ApiProgress.js
+++ src/shared/ApiProgress.js
@@ -16,18 +16,23 @@ export function withApiProgress(WrappedComponent,apiPath){
     
         
         componentDidMount() {
-            axios.interceptors.request.use(request => {
+            this.requestInterceptor = axios.interceptors.request.use(request => {
              this.updateApiCallFor(request.url,true);
                 return request;
             });
     
-            axios.interceptors.response.use((response) => {
+           this.responseInterceptor =  axios.interceptors.response.use((response) => {
                 this.updateApiCallFor(response.url,false);
                 return response;
             },(error) => {
                 this.updateApiCallFor(error.config.url,false);
                 throw error;
             })
+        }
+
+        componentWillUnmount (){
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.request.eject(this.responseInterceptor);
         }
     
         updateApiCallFor = (url,inProgress) => {
