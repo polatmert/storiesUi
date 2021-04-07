eba35bd3483406271d53ebef534717b912b6923d
MertPolat <husnumertpolatt@gmail.com>
2021-04-07 21:24:42 +0300

    TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,24 +4,46 @@ import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 
 class TopBar extends Component {
+state = {
+    isLoggedIn:true,
+    username:'user1'
+}
+
     render() {
         const { t } = this.props;
+        const { isLoggedIn, username } = this.state;
+
+        let links = (
+            <ul className="navbar-nav ml-auto">
+                <li>
+                    <Link className="nav-link" to="/login">
+                        {t('Login')}
+                    </Link>
+                </li>
+                <Link className="nav-link" to="/signUp">
+                    {t('Sign Up')}
+                </Link>
+            </ul>
+        );
+
+        if (isLoggedIn) {
+            links = (
+                <ul className="navbar-nav ml-auto">
+                    <Link className="nav-link" to = {`/user/${username}`}>
+                        {username}
+                    </Link>
+                    <li className="nav-link">{t('Logout')}</li>
+                </ul>
+            )
+        };
+      
         return (
             <div className="shadow-sm bg-light mb-2">
                 <nav className="navbar navbar-light container navbar-expand">
                     <Link className="navbar-brand" to="/">
-                        <img src={logo} width="80" alt="StoryLogo"/>Story
+                        <img src={logo} width="80" alt="StoryLogo" />Story
                     </Link>
-                    <ul className="navbar-nav ml-auto">
-                        <li>
-                            <Link className = "nav-link" to="/login">
-                            {t('Login')}
-                            </Link>
-                        </li>
-                           <Link className = "nav-link" to="/signUp">
-                            {t('Sign Up')}
-                           </Link>                    
-                    </ul>
+                        {links}
                 </nav>
             </div>
         );
